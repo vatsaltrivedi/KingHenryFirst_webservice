ba4e8dca7f38f7b58864d316078e8c9bee3e3f9c
Vatsal <bhaktvatsal@gmail.com>
2015-11-07 00:25:17 +0000

    added get requests

--- app.py
+++ app.py
@@ -5,12 +5,46 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Flask is running!'
+    return 'King Henry First Web Service'
 
+# Get the latest rate: "127.0.0.1/rate" or http://domainname/rate
+# similar url for all the methods or services
 
+@app.route("/rate")
+def rate():
+	return requests.get('https://shapeshift.io/rate/btc_ltc').content
+
+#Get List of Supported Coins with Icon Links
+@app.route('/coins')
+def status():
+	return requests.get('https://shapeshift.io/getcoins').content
+
+#deposit limit
+@app.route("/limit")
+def rate():
+	return requests.get('https://shapeshift.io/limit/btc_ltc').content
+
+#market Info
 @app.route('/marketinfo')
-def names():
+def marketinfo():
     return requests.get('https://shapeshift.io/marketinfo/btc_ltc').content
+
+# Recent Transaction [10]
+@app.route('/transaction')
+def transaction():
+	return requests.get('https://shapeshift.io/recenttx/10').content
+
+#Status of deposit to address
+@app.route('/status/<address>')
+def status(username):
+	return requests.get('https://shapeshift.io/txStat/%s' %address).content
+
+#Time Remaining on Fixed Amount Transaction
+@app.route('/timeremaining/<address>')
+def status(username):
+	return requests.get('https://shapeshift.io/timeremaining/%s' %address).content
+
+
 
 if __name__ == '__main__':
     app.run()
